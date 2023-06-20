816e0aba678725ef648fdcbf26530ca47563c714
AndreD4 <andremai629@gmail.com>
2023-06-20 13:32:09 +0900

    move enemy and flip

--- Assets/Script/EnemyMovment.cs
+++ Assets/Script/EnemyMovment.cs
@@ -1,22 +1,34 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class EnemyMovment : MonoBehaviour
 {
-    [SerializeField] float moveSpeed = 1f;
-    Rigidbody2D myRb;
+  [SerializeField] float moveSpeed = 1f;
+  Rigidbody2D myRb;
 
 
 
-    void Start()
-    {
-        myRb = GetComponent<Rigidbody2D>();
-    }
+  void Start()
+  {
+    myRb = GetComponent<Rigidbody2D>();
+  }
 
-    
-    void Update()
-    {
-        myRb.velocity = new Vector2 (moveSpeed, 0f);
-    }
+
+  void Update()
+  {
+    myRb.velocity = new Vector2(moveSpeed, 0f);
+  }
+
+  void OnTriggerExit2D(Collider2D other)
+  {
+    moveSpeed = -moveSpeed;
+    FlipEnemyFacing();
+  }
+
+  void FlipEnemyFacing()
+  {
+    transform.localScale = new Vector2 (-(Mathf.Sign(myRb.velocity.x)), 1f);
+  }
 }
