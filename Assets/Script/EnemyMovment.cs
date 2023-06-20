34a7f3335555ae157c649d5d1a4c6fb5414eaed9
AndreD4 <andremai629@gmail.com>
2023-06-20 13:16:48 +0900

    basic movment

--- Assets/Script/EnemyMovment.cs
+++ Assets/Script/EnemyMovment.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class EnemyMovment : MonoBehaviour
 {
-    
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRb;
+
+
+
     void Start()
     {
-        
+        myRb = GetComponent<Rigidbody2D>();
     }
 
     
     void Update()
     {
-        
+        myRb.velocity = new Vector2 (moveSpeed, 0f);
     }
 }
